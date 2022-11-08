c233eb678955d3bc3b001096fc3d958d0c846ea9
cwiley982 <cwiley982@gmail.com>
2022-11-08 11:18:36 -0500

    Icon now uses outlined car icon
    Also cleaned up all the resources for legacy icon support, aka got
    rid of those so only the adaptive icon is available now.

--- app/src/main/kotlin/com/caitlynwiley/aapparkingsaver/theme/Color.kt
+++ app/src/main/kotlin/com/caitlynwiley/aapparkingsaver/theme/Color.kt
@@ -2,14 +2,6 @@ package com.caitlynwiley.aapparkingsaver.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
-
 // Custom App Colors //
 val RelayDarkBlue = Color(0xFF102A3C)
 val RelayTeal = Color(0xFF4EAEB8)
@@ -19,6 +11,6 @@ val Level3Blue = Color(0xFF02657E)
 val Level4Yellow = Color(0xFFE7AA06)
 val Level5Green = Color(0xFF007C43)
 val Level6Purple = Color(0xFF731364)
-val Level7Red = Color(0xFFC92403)
-val Level8Blue = Color(0xFF1299DB)
-val Level9Yellow = Color(0xFFD8BA2F)
+val Level7Red = Color(0xFF9F2208)
+val Level8Blue = Color(0xFF468CB2)
+val Level9Yellow = Color(0xFFDAB611)
